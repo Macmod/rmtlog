8d73c75975a41b802a53358eb305db0a11fd2a2b
Macmod <zz.mcmd@gmail.com>
2018-09-05 21:34:31 -0300

    Adding files.

---
+++ ack.h
@@ -0,0 +1,11 @@
+#ifndef ACK_H
+#define ACK_H
+#include <signal.h>
+#include "slidingwindow.h"
+
+void unset_ack_timeout(SlidingWindowElem*);
+void set_ack_timeout(SlidingWindowElem*, uint64_t);
+void ack_timeout(union sigval);
+void set_ack_flag(uint64_t, SlidingWindow*);
+
+#endif
